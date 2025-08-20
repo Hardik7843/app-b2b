6d65f45b94dc8c095ac26adebcd629541e45ca28
Hardik-BlkCrnt <hardikp.blackcurrant@gmail.com>
2025-08-20 17:35:28 +0530

    first commit

---
+++ src/app/(user)/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import React from 'react'
+import 
+const page = () => {
+  return (
+    <div className='h-screen'>
+        <Image src={}></Image>
+    </div>
+  )
+}
+
+export default page
