ee6ffd3f9f02e2cfb9d00a730575601e2d73ebb9
Hardik-BlkCrnt <hardikp.blackcurrant@gmail.com>
2025-08-20 16:51:35 +0530

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Meal Hub",
+  description: "Making Dine-in & Takeaways an ease",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <div className="h-screen bg-white text-black">
+        {children}
+        </div>
+      </body>
+    </html>
+  );
+}
