167c39e9ada8b09adf79bd19641814e991c71d26
Hardik7843 <hardikprajapati7843@gmail.com>
2025-08-20 23:54:51 +0530

    added image

--- src/app/(user)/(auth)/login/page.tsx
+++ src/app/(user)/(auth)/login/page.tsx
@@ -1,12 +1,31 @@
-import Image from 'next/image'
-import React from 'react'
-import 
+import { masterImageBackgrounArt2 } from "@/app/static/imageImports";
+import { masterImageBackgrounArt1 } from "@/app/static/imageImports";
+import Image from "next/image";
+import React from "react";
+
 const page = () => {
   return (
-    <div className='h-screen'>
-        <Image src={}></Image>
+    <div className="w-screen">
+      <div className="p-5">
+        {/* <div className="relative aspect-square w-full">
+          <Image
+            className="object-cover"
+            src={masterImageBackgrounArt1}
+            fill
+            alt="ImageArt1"
+          />
+        </div> */}
+        <div className="relative aspect-square w-ful;">
+          <Image
+            className="object-cover"
+            src={masterImageBackgrounArt2}
+            fill
+            alt="ImageArt2"
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
---
+++ src/app/static/imageImports.ts
@@ -0,0 +1,5 @@
+import bgArt2 from "@/../public/user/Ilustration.svg";
+import bgArt1 from "@/../public/user/Pattern.svg";
+
+export const masterImageBackgrounArt1 = bgArt1;
+export const masterImageBackgrounArt2 = bgArt2;
